330e398d337150d6f05d1fc2bfa978f048717b93
Maria-Faria <mariaeduardadefaria15@gmail.com>
2025-02-28 19:15:47 -0300

    fix: allowedOptions

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import cors from 'cors';
 
 const app = express();
-const allowedOptions = ['http://localhost:3000'];
+const allowedOptions = ['http://localhost:3000', 'https://zoonoses-front.vercel.app'];
 
 app.use(cors({
   origin: allowedOptions
