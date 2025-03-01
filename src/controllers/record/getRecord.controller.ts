f39080b7d0e7e57e990cf3f512eb8dddec2b7e1e
Maria-Faria <mariaeduardadefaria15@gmail.com>
2025-02-28 21:58:53 -0300

    fix: date format

--- src/controllers/record/getRecord.controller.ts
+++ src/controllers/record/getRecord.controller.ts
@@ -37,7 +37,7 @@ const getRecordController = async(req: Request, res: Response): Promise<any> => 
       })
     }
 
-    return res.status(200).json({tutor: {cpf: tutor?.cpf, name: tutor?.name, cep: address?.cep, number: address?.number, phone: tutor?.phone}, pet: {microchip: pet?.microchip, age: pet?.age, size: pet?.size_pet, inputDate: moment(pet?.input_date).format("DD/MM/YYYY"), specie: pet?.specie, breed: pet?.breed, color: pet?.color, weight: pet?.weight, gender: pet?.pet_gender, plate: pet?.plate}, services: servicesList, totalPrice: record.price});
+    return res.status(200).json({tutor: {cpf: tutor?.cpf, name: tutor?.name, cep: address?.cep, number: address?.number, phone: tutor?.phone}, pet: {microchip: pet?.microchip, age: pet?.age, size: pet?.size_pet, inputDate: moment(pet?.input_date).format("YYYY-MM-DD"), specie: pet?.specie, breed: pet?.breed, color: pet?.color, weight: pet?.weight, gender: pet?.pet_gender, plate: pet?.plate}, services: servicesList, totalPrice: record.price});
     
   } catch (error) {
     return res.status(500).json({error: `${error} - Erro interno de servidor`});
